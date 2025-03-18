cafccf0eac5499136e55dc4cc79de5133e768446
anapaulasuassuna <anapaulaenf.ce@gmail.com>
2025-03-18 12:02:39 -0300

    aula 4

---
+++ mod-08/main.py
@@ -0,0 +1,5 @@
+def hello(name):
+    print(f'Olá, {name}')
+    print(f'Como você está, {name}')
+
+hello('Alisson')
---
+++ mod-08/task.py
@@ -0,0 +1,15 @@
+# def hello(name):
+#     print(f'Olá, {name}')
+#     print(f'Como você está, {name}')
+#
+# hello('Alisson')
+
+# TODO Crie uma função chamada saudacao que receba como parâmetros nome e cidade,
+#  quando chamada mostre no console a mensagem: "Olá Alisson, onde você mora: Juazeiro do norte - CE"
+
+def saudacao(nome, cidade):
+    print(f"Olá {nome}")
+    print(f"Onde você mora: {cidade}")
+
+
+saudacao(cidade="Reino Celestial", nome="Jesus Cristo")
